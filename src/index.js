f0aeab13b7f83e547d2b4d138052078011a48940
Matara Kemari <drkemari@gmail.com>
2025-07-08 11:07:32 +0300

    Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const description = e.target['new-task-description'].value;
+    const user = e.target['user'].value;
+    const duration = e.target['duration'].value;
+    const dueDate = e.target['dueDate'].value;
+    const priority = e.target['priority'].value;
+    const taskDetails = `
+    <strong>Task:</strong> ${description} | 
+    <strong>User:</strong> ${user} | 
+    <strong>Duration:</strong> ${duration} hrs | 
+    <strong>Due:</strong> ${dueDate} | 
+    <strong>Priority:</strong> ${priority} `;
+    buildToDo(taskDetails,priority);
+    form.reset();
+  });
+}); 
+
+function buildToDo(todo,priority) {
+  const li = document.createElement('li');  
+  const btn = document.createElement('button');
+
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  btn.style.color='white';
+  btn.style.backgroundColor='red';
+  btn.style.fontSize='15px';
+  li.innerHTML = todo;
+ //Set color based on priority
+ switch(priority.toLowerCase()) {
+  case 'high':
+    li.style.color = 'red';
+    break;
+  case 'medium':
+    li.style.color = 'orange';
+    break;
+  case 'low':
+    li.style.color = 'green';
+    break;
+}
+
+  li.appendChild(btn);
+
+  console.log(li); 
+
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
